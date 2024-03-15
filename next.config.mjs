a7b432c8c3a9caf83ec6efcf00ff363f91bce6c8
Bronislav Musil <brona.musil@gmail.com>
2024-03-15 15:40:42 +0100

    Root

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    i18n: {
+        locales: ['default', "cs", "en"],
+        defaultLocale: "cs",
+        localeDetection: false,
+    },
+};
 
 export default nextConfig;
